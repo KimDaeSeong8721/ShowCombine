8a32e45514d15ff13a5768124aa2a52ff454e25d
Miller <eotjd8721@naver.com>
2022-05-02 20:41:23 +0900

    first commit

---
+++ ShowCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShowCombine
+//
+//  Created by DaeSeong on 2022/05/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShowCombine/ShowCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowCombineApp.swift
+//  ShowCombine
+//
+//  Created by DaeSeong on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct ShowCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
